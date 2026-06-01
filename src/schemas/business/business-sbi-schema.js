1d428526a33d5edf1c50f03e7125153aa94adc79
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2026-06-01 17:30:48 +0100

    Update business SBI schema and tests (#18)
    The SBI had an invalid format during validation.
    This PR fixed the format to be 9 digits and fixes the error message.

--- src/schemas/business/business-sbi-schema.js
+++ src/schemas/business/business-sbi-schema.js
@@ -2,10 +2,10 @@ import Joi from 'joi'
 
 export const businessSbiSchema = Joi.object({
   sbi: Joi.string()
-    .pattern(/^\d{10}$/)
+    .pattern(/^\d{9}$/)
     .allow('')
     .optional()
     .messages({
-      'string.pattern.base': 'Enter a SBI number, like 1234567891'
+      'string.pattern.base': 'Enter the full SBI'
     })
 })
