c9d1598177fca733c55d00f28819ff9895627d44
Rebecca Ransome <beckyrose200@aol.com>
2026-07-09 21:12:09 +0100

    Add: Comment to business schema

--- src/schemas/business/business-schemas.js
+++ src/schemas/business/business-schemas.js
@@ -5,11 +5,19 @@ import { businessEmailSchema } from './business-email-schema.js'
 import { businessPhoneSchema } from './business-phone-schema.js'
 import { businessVatSchema } from './business-vat-schema.js'
 
+/**
+ * The businessSchema object has a nested property of `details`.
+ * This is due to the interrupter journey. The journey required the business details to be validated in a single step,
+ * these include name, address, phone, email and vat. The `details` property is used to group these schemas together.
+ * The `sbi` property is separate as it is validated in a different step of the journey.
+ */
 export const businessSchemas = {
   sbi: businessSbiSchema,
-  address: addressSchema,
-  name: businessNameSchema,
-  phone: businessPhoneSchema,
-  email: businessEmailSchema,
-  vat: businessVatSchema
+  details: {
+    name: businessNameSchema,
+    address: addressSchema,
+    phone: businessPhoneSchema,
+    email: businessEmailSchema,
+    vat: businessVatSchema
+  }
 }
