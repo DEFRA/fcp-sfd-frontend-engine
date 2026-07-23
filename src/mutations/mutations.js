d9fe6452d485c297d5821bbead2a8db8532debd6
Rana Salem <127218837+rtasalem@users.noreply.github.com>
2026-07-23 13:13:18 +0100

    FLS2-47 Add mutation for updating business email (#53)

--- src/mutations/mutations.js
+++ src/mutations/mutations.js
@@ -1,5 +1,7 @@
+import { updateBusinessEmailMutation } from './business/update-business-email.js'
 import { updateCustomerNameMutation } from './personal/update-customer-name.js'
 
 export const mutations = {
+  updateBusinessEmail: updateBusinessEmailMutation,
   updateCustomerName: updateCustomerNameMutation
 }
