0b488c70277210e561f6a40040788e165b00bc50
Adam Kay <thousand_leaves@hotmail.com>
2026-07-23 15:08:25 +0100

    Add Shared Personal Email Mutation

--- src/mutations/mutations.js
+++ src/mutations/mutations.js
@@ -1,5 +1,7 @@
 import { updateCustomerNameMutation } from './personal/update-customer-name.js'
+import { updateCustomerEmailMutation } from './personal/update-customer-email.js'
 
 export const mutations = {
-  updateCustomerName: updateCustomerNameMutation
+  updateCustomerName: updateCustomerNameMutation,
+  updateCustomerEmail: updateCustomerEmailMutation
 }
