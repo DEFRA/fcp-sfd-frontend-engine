4d1ac848f96fa10862fc3c458cd306f758f1aea9
Adam Kay <90856315+thousand-leaves@users.noreply.github.com>
2026-09-02 14:55:28 +0100

    Add Remaining Success Message Constants (#86)
    * Add Remaining Success Message Constants For Business And Personal Journeys
    
    * Bump version to 0.31.2 in package.json and package-lock.json
    
    * add legal status related successages

--- test/unit/constants/success-messages.test.js
+++ test/unit/constants/success-messages.test.js
@@ -3,11 +3,20 @@ import { describe, test, expect } from 'vitest'
 
 // Thing under test
 import {
+  BUSINESS_ADDRESS,
+  BUSINESS_CHARITY_REGISTRATION_NUMBER,
+  BUSINESS_COMPANY_REGISTRATION_NUMBER,
   BUSINESS_EMAIL_ADDRESS,
+  BUSINESS_LEGAL_STATUS,
   BUSINESS_NAME,
   BUSINESS_PHONE_NUMBERS,
   BUSINESS_VAT,
-  BUSINESS_VAT_REMOVE
+  BUSINESS_VAT_REMOVE,
+  PERSONAL_ADDRESS,
+  PERSONAL_DOB,
+  PERSONAL_EMAIL_ADDRESS,
+  PERSONAL_NAME,
+  PERSONAL_PHONE_NUMBERS
 } from '../../../src/constants/success-messages.js'
 
 describe('success messages', () => {
@@ -29,5 +38,41 @@ describe('success messages', () => {
 
   test('BUSINESS_VAT_REMOVE has the expected value', () => {
     expect(BUSINESS_VAT_REMOVE).toEqual('You have removed your VAT registration number')
+  })
+
+  test('BUSINESS_ADDRESS has the expected value', () => {
+    expect(BUSINESS_ADDRESS).toEqual('You have updated your business address')
+  })
+
+  test('BUSINESS_CHARITY_REGISTRATION_NUMBER has the expected value', () => {
+    expect(BUSINESS_CHARITY_REGISTRATION_NUMBER).toEqual('You have updated your charity commission registration number')
+  })
+
+  test('BUSINESS_COMPANY_REGISTRATION_NUMBER has the expected value', () => {
+    expect(BUSINESS_COMPANY_REGISTRATION_NUMBER).toEqual('You have updated your company registration number')
+  })
+
+  test('BUSINESS_LEGAL_STATUS has the expected value', () => {
+    expect(BUSINESS_LEGAL_STATUS).toEqual('You have updated your business legal status')
+  })
+
+  test('PERSONAL_NAME has the expected value', () => {
+    expect(PERSONAL_NAME).toEqual('You have updated your full name')
+  })
+
+  test('PERSONAL_EMAIL_ADDRESS has the expected value', () => {
+    expect(PERSONAL_EMAIL_ADDRESS).toEqual('You have updated your personal email address')
+  })
+
+  test('PERSONAL_PHONE_NUMBERS has the expected value', () => {
+    expect(PERSONAL_PHONE_NUMBERS).toEqual('You have updated your personal phone numbers')
+  })
+
+  test('PERSONAL_DOB has the expected value', () => {
+    expect(PERSONAL_DOB).toEqual('You have updated your date of birth')
+  })
+
+  test('PERSONAL_ADDRESS has the expected value', () => {
+    expect(PERSONAL_ADDRESS).toEqual('You have updated your personal address')
   })
 })
