a78b68e0c9b84807dbbe4ba6e2cc644aae1fbad7
Adam Kay <thousand_leaves@hotmail.com>
2026-09-21 10:07:47 +0100

    docs: add documentation comments for business and customer details mutations

--- src/mutations/business/update-business-details.js
+++ src/mutations/business/update-business-details.js
@@ -1,3 +1,7 @@
+/**
+ * Updates any combination of business name, email, phone, VAT and address.
+ */
+
 export const updateBusinessDetailsMutation = `
   mutation UpdateBusinessAllFields($input: UpdateBusinessAllFieldsInput!) {
     updateBusinessAllFields(input: $input) {
--- src/mutations/personal/update-customer-details.js
+++ src/mutations/personal/update-customer-details.js
@@ -1,3 +1,7 @@
+/**
+ * Updates any combination of personal name, email, phone, and address.
+ */
+
 export const updateCustomerDetailsMutation = `
   mutation UpdateCustomerAllFields($input: UpdateCustomerAllFieldsInput!) {
     updateCustomerAllFields(input: $input) {
